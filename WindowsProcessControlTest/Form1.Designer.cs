e54bce2b68a59b9bccdbb291ed6b61d21153a224
Coldest Chen <brian824100@gmail.com>
2017-05-01 00:38:40 +0800

    test 1

--- WindowsProcessControlTest/Form1.Designer.cs
+++ WindowsProcessControlTest/Form1.Designer.cs
@@ -173,7 +173,7 @@
             this.Controls.Add(this.btnClose);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Process Contorl";
+            this.Text = "Process Contorl Test";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
